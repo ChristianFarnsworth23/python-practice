e4fb822356fc9d2ab54744e3adcac2791d07cdb3
ChristianFarnsworth23 <834243@mystma.org>
2021-09-14 14:27:36 -0500

    Added sign in info

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("Why is it so dark?")
+print("I hope it won't be dark for a long time")
+
+#Print 1st
+print("The Suicide Squad")
+
+#Print 2nd
+print("Rick and Morty")
+
+#Print 3rd
+print("Invincible")
